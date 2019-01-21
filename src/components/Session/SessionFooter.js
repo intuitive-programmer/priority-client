cca5ddee2316890f9302b37f34a86fb8ae216311
Steven Rico <steven@intuitiveprogrammer.com>
2019-01-21 14:10:30 +0000

    Added sortBtn to SessionFooter, restructured into a class component.

--- src/components/Session/SessionFooter.js
+++ src/components/Session/SessionFooter.js
@@ -1,25 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import { Button } from '@material-ui/core'
 
-const SessionFooter = ({ match, history }) => {
-  const handleNextStepClick = () => {
+class SessionFooter extends Component {
+  state = {
+    displaySortBtn: true
+  }
+
+  handleNextStepClick = () => {
+    const { match, history } = this.props
     const currentScene = match.url.split('/')[2]
     switch (currentScene) {
       case 'note':
       return history.push('/app/vote/start')
+      case 'vote':
+      return console.log("next bit")
       default:
       return null
     }
   }
 
-  return(
-    <footer className="session-footer flex-center top-shadow">
-      <Button
-        onClick={() => handleNextStepClick()}
-        variant="outlined"
-        color="primary">Next Step</Button>
-    </footer>
+  render() {
+    const { displaySortBtn } = this.state
+    const { match } = this.props
+    return(
+      <footer className="session-footer flex-center top-shadow">
+        {match.url === '/app/vote/review' && displaySortBtn
+          ? this.renderSortBtn()
+          : this.renderNextStepBtn()
+        }
+      </footer>
+    )
+  }
+
+  renderSortBtn = () => (
+    <Button
+      variant="contained"
+      color="primary"
+      onClick={() => this.setState({ displaySortBtn: false })}
+    >Sort By Priority</Button>
+  )
+
+  renderNextStepBtn = () => (
+    <Button
+      onClick={() => this.handleNextStepClick()}
+      variant="outlined"
+      color="primary"
+    >Next Step</Button>
   )
 }
 
